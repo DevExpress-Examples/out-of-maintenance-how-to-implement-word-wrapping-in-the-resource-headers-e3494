734c8896e5c50e2eeeb505f07bb7a1a9eba346b3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/App_Code/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Web.ASPxScheduler;
+using DevExpress.XtraScheduler;
+
+public class ResourceFiller {
+    public static string[] Users = new string[] { "Sarah Brighton", "Ryan Fischer", "Andrew Miller", "Barbara Chinavare", "Lewis Borrmann", "Alberta Berntsen" };
+    public static string[] Usernames = new string[] { "sbrighton", "rfischer", "amiller", "bchinavare", "lborrmann", "aberntsen" };
+
+    public static void FillResources(ASPxSchedulerStorage storage) {
+        ResourceCollection resources = storage.Resources.Items;
+        storage.BeginUpdate();
+        try {
+            for (int i = 1; i <= Users.Length; i++) {
+                resources.Add(new Resource(Usernames[i - 1], Users[i - 1]));
+            }
+        }
+        finally {
+            storage.EndUpdate();
+        }
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,76 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.XtraScheduler
+Imports DevExpress.Web.ASPxScheduler
+
+Partial Public Class [Default]
+	Inherits System.Web.UI.Page
+	Private ReadOnly Property Storage() As ASPxSchedulerStorage
+		Get
+			Return ASPxScheduler1.Storage
+		End Get
+	End Property
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		SetupMappings()
+		ResourceFiller.FillResources(Me.ASPxScheduler1.Storage)
+
+		ASPxScheduler1.AppointmentDataSource = appointmentDataSource
+		ASPxScheduler1.DataBind()
+
+		ASPxScheduler1.GroupType = SchedulerGroupType.Resource
+	End Sub
+	Private Sub SetupMappings()
+		Dim mappings As ASPxAppointmentMappingInfo = Storage.Appointments.Mappings
+		Storage.BeginUpdate()
+		Try
+			mappings.AppointmentId = "Id"
+			mappings.Start = "StartTime"
+			mappings.End = "EndTime"
+			mappings.Subject = "Subject"
+			mappings.AllDay = "AllDay"
+			mappings.Description = "Description"
+			mappings.Label = "Label"
+			mappings.Location = "Location"
+			mappings.RecurrenceInfo = "RecurrenceInfo"
+			mappings.ReminderInfo = "ReminderInfo"
+			mappings.ResourceId = "OwnerId"
+			mappings.Status = "Status"
+			mappings.Type = "EventType"
+		Finally
+			Storage.EndUpdate()
+		End Try
+	End Sub
+	'Initilazing ObjectDataSource
+	Protected Sub appointmentsDataSource_ObjectCreated(ByVal sender As Object, ByVal e As ObjectDataSourceEventArgs)
+		e.ObjectInstance = New CustomEventDataSource(GetCustomEvents())
+	End Sub
+	Private Function GetCustomEvents() As CustomEventList
+		Dim events As CustomEventList = TryCast(Session("ListBoundModeObjects"), CustomEventList)
+		If events Is Nothing Then
+			events = New CustomEventList()
+			Session("ListBoundModeObjects") = events
+		End If
+		Return events
+	End Function
+
+	' User generated appointment id    
+	Protected Sub ASPxScheduler1_AppointmentInserting(ByVal sender As Object, ByVal e As PersistentObjectCancelEventArgs)
+		SetAppointmentId(sender, e)
+	End Sub
+
+	Private Sub SetAppointmentId(ByVal sender As Object, ByVal e As PersistentObjectCancelEventArgs)
+		Dim storage As ASPxSchedulerStorage = CType(sender, ASPxSchedulerStorage)
+		Dim apt As Appointment = CType(e.Object, Appointment)
+		storage.SetAppointmentId(apt, apt.GetHashCode())
+	End Sub
+End Class
